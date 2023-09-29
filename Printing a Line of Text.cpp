db06266e25793645fa84ea881a6da21a2fabed46
Enes067119 <146369763+Enes067119@users.noreply.github.com>
2023-09-29 12:09:06 +0300

    Printing a Line of Text

---
+++ Printing a Line of Text.cpp
@@ -0,0 +1,8 @@
+#include <iostream> // allows program to output data to the screen
+
+int main() // function main begins program execution
+{
+	std::cout << "Welcome to C++!\n"; // display message
+	
+	return 0; /* indicate that program ended succesfully */
+}
