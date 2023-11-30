5b4ce79bf4f6b1f681bf04cdc0244a8bca958789
Enes067119 <146369763+Enes067119@users.noreply.github.com>
2023-11-30 16:49:01 +0300

    Add files via upload

---
+++ Placing a Class in a Separate File for Reusability/Main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "GradeBook.h"
+using namespace std;
+
+int main()
+	{
+		GradeBook gradeBook1("CS101 Introduction to C++ Programming");
+		GradeBook gradeBook2("CS102 Data Structures in C++");
+		
+		cout << "gradeBook1 created for course: " << gradeBook1.getCourseName()
+		<< "\ngradeBook2 created for course: " << gradeBook2.getCourseName()
+		<< endl;
+		
+	}
